5e3bd2b890a95d61eb5af2e6a6a95e850299dd73
Arman Hashemi <ahhdev@gmail.com>
2022-08-29 20:45:44 +0430

    enable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
+import node from '@astrojs/node';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node(),
   integrations: [compress(), image(), react(), tailwind()]
 });
