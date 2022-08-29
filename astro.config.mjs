8e37466c521b45b118af7f11611f0292ab5d3e8a
Arman Hashemi <ahhdev@gmail.com>
2022-08-29 20:12:21 +0430

    add astro-image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import compress from "astro-compress";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
+  integrations: [compress(), image()]
 });
