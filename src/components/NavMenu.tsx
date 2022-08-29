37e0a154ab610f5f9ad5e50bcf4f65043d8aaf33
Arman Hashemi <ahhdev@gmail.com>
2022-08-29 21:31:11 +0430

    remove public iamges

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,72 +1,72 @@
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 
-const NavMenu =  () => (
-  <NavigationMenu.Root>
-    <NavigationMenu.List>
-      <NavigationMenu.Item>
+export default () => { 
+  return(
+    <NavigationMenu.Root>
+      <NavigationMenu.List>
+        <NavigationMenu.Item>
+            <NavigationMenu.Trigger>Item</NavigationMenu.Trigger>
+          <NavigationMenu.Content>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item>
           <NavigationMenu.Trigger>Item</NavigationMenu.Trigger>
-        <NavigationMenu.Content>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-        </NavigationMenu.Content>
-      </NavigationMenu.Item>
+          <NavigationMenu.Content>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
 
-      <NavigationMenu.Item>
-        <NavigationMenu.Trigger>Item</NavigationMenu.Trigger>
-        <NavigationMenu.Content>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-        </NavigationMenu.Content>
-      </NavigationMenu.Item>
-
-      <NavigationMenu.Item>
-        <NavigationMenu.Trigger>Item</NavigationMenu.Trigger>
-        <NavigationMenu.Content>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-          <div>
-          Sub item 1
-          </div>
-        </NavigationMenu.Content>
-      </NavigationMenu.Item>
-    </NavigationMenu.List>
-  </NavigationMenu.Root>
-);
-
-export default NavMenu
+        <NavigationMenu.Item>
+          <NavigationMenu.Trigger>Item</NavigationMenu.Trigger>
+          <NavigationMenu.Content>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+            <div>
+            Sub item 1
+            </div>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
+  )
+ };
