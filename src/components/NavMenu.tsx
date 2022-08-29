107f22d8baa8f4da19e9128ce31fc064b6627129
Arman Hashemi <ahhdev@gmail.com>
2022-08-29 20:42:04 +0430

    add test

---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,70 @@
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+
+export default () => (
+  <NavigationMenu.Root>
+    <NavigationMenu.List>
+      <NavigationMenu.Item>
+          <NavigationMenu.Trigger>Item</NavigationMenu.Trigger>
+        <NavigationMenu.Content>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+        </NavigationMenu.Content>
+      </NavigationMenu.Item>
+
+      <NavigationMenu.Item>
+        <NavigationMenu.Trigger>Item</NavigationMenu.Trigger>
+        <NavigationMenu.Content>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+        </NavigationMenu.Content>
+      </NavigationMenu.Item>
+
+      <NavigationMenu.Item>
+        <NavigationMenu.Trigger>Item</NavigationMenu.Trigger>
+        <NavigationMenu.Content>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+          <div>
+          Sub item 1
+          </div>
+        </NavigationMenu.Content>
+      </NavigationMenu.Item>
+    </NavigationMenu.List>
+  </NavigationMenu.Root>
+);
