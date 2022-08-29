8fb53066f4451bc911787ac07d69323957e47d90
Arman Hashemi <ahhdev@gmail.com>
2022-08-29 20:16:19 +0430

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 import image from "@astrojs/image";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), image(), react()]
+  integrations: [compress(), image(), react(), tailwind()]
 });
