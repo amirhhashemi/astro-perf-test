8edbe1a844597ae3f757bd3e50ee4afd47706824
Arman Hashemi <ahhdev@gmail.com>
2022-08-29 20:14:57 +0430

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
-
 import image from "@astrojs/image";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), image()]
+  integrations: [compress(), image(), react()]
 });
